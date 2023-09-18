2e4b09613859b60c945ae9289c769a16c0e1ebc7
Marcos Junior <marcosantonio.jose01@gmail.com>
2023-09-18 16:56:30 -0300

    add submit feedbacks to form

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,33 @@ import { useState } from 'react';
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
+  const [isErrorMessageVisible, setIsErrorMessageVisible] = useState(false);
+
+  const handleSubmitForm = async () => {
+    setIsLoadingSubmit(true);
+    setIsErrorMessageVisible(false);
+    try {
+      const data = {
+        email,
+        password,
+      }
+  
+      await login(data);
+      alert("Login realizado com sucesso!");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      setIsErrorMessageVisible(true);
+    }
+    setIsLoadingSubmit(false);
+  };
 
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
-        <div className='errorMessage'></div>
+        <div className='errorMessage' style={{display: isErrorMessageVisible ? "block" : "none"}}>E-mail ou senha inválidos</div>
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
           <input id={'email'} type={'email'} value={email} onChange={(e) => setEmail(e.target.value)} autoComplete='off' />
@@ -22,9 +43,10 @@ export default function LoginForm() {
 
         <div className='button'>
           <button 
-            disabled={!email || !password || password.length < 6}
+            disabled={!email || !password || password.length < 6 || isLoadingSubmit}
+            onClick={handleSubmitForm}
           >
-            Login
+            {isLoadingSubmit ? "Carregando..." : "Login"}
           </button>
         </div>
       </div>
